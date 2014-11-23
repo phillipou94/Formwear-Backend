f2a2321de6fa9e515e001b8dce42514aefc2a299
phillipou94 <phillipou@Phillips-MacBook-Pro-2.local>
2014-11-23 13:16:12 -0800

    sort by date

--- fashion_web.js
+++ fashion_web.js
@@ -135,7 +135,7 @@ app.get('/items',function(req,res){
 //get request for items this user liked
 app.get('/items/:id',function(req,res){
 	var collection = db.collection("items")
-	collection.find({"likedBy": { $in: [req.params.id ] }},{}).toArray(function(e,results){
+	collection.find({"likedBy": { $in: [req.params.id ] }},{}).sort({dateCreated:-1}).toArray(function(e,results){
 		console.log(e);
 		if(e) res.status(500).send()
 			res.send(results)
